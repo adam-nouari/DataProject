93a893464dbc156e521fd271697b79f44b4d005b
Sachachen <102303534+Sachachen@users.noreply.github.com>
2025-11-16 23:18:53 +0100

    Correction

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -10,7 +10,7 @@ from .home import create_app
 from .simple_page import layout as simple_layout
 from .simple_page import register_callbacks as simple_callbacks
 from .create_geo_loc import layout as geo_layout
-from .create_geo_loc import create_choropleth
+from .create_geo_loc import creer_carte as create_choropleth
 
 __all__ = [
     "create_app",
